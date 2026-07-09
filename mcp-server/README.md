3de92dcc0ee08d6bb7c24b70931a5d85d029dce6
Brandon Ha <idevbrandon@gmail.com>
2026-07-09 14:28:05 -0400

    running mcp

---
+++ mcp-server/README.md
@@ -0,0 +1,5 @@
+# MCP practice
+
+How to run
+
+npx @modelcontextprotocol/inspector npx -y tsx main.ts
--- mcp-server/main.ts
+++ mcp-server/main.ts
@@ -1,27 +1,31 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 
 const server = new McpServer({
-  name: "oxinion-weather",
+  name: "oxinion-finance",
   version: "1.0.0",
 });
 
 server.registerTool(
-  "get-weather",
+  "get-stock-info",
   {
-    description: "Get the current weather for a given location",
+    description: "Get the current stock information for a given symbol",
     inputSchema: z.object({
-      city: z.string().describe("The name of the city to get the weather for"),
+      symbol: z.string().describe("The stock symbol to get information for"),
     }),
   },
-  async ({ city }) => {
+  async ({ symbol }) => {
     return {
       content: [
         {
           type: "text",
-          text: `The current weather in ${city} is sunny with a temperature of 25°C.`,
+          text: `The current price for ${symbol} is $150.00 .`,
         },
       ],
     };
   },
 );
+
+const transport = new StdioServerTransport();
+server.connect(transport);
